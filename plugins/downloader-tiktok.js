1456b833cdc3e69cccdd564ec77dd88cb2df193d
Lu asik bang <98279739+NdaaaBilek@users.noreply.github.com>
2023-05-16 12:37:24 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,29 +1,10 @@
-var fetch = require("node-fetch");
-var handler = async (m, {
-	conn,
-	args,
-	usedPrefix,
-	command
-}) => {
-if (!args[0]) throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
-if (!args[0].match(/tiktok/gi)) throw `URL Tidak Ditemukan!`
-m.reply(wait)
- var apis = await fetch(`https://api.botcahx.live/api/dowloader/tiktok?url=${args[0]}&apikey=${btc}`)
-if (!apis.ok) throw await apis.text()
-var jsons = await apis.json()
-if (!jsons.status) throw jsons
-var { 
-video, 
-video2, 
-username,
-description,
-audio
-} = jsons.result
-await conn.sendFile(m.chat, video, 'tiktok.mp4', `
-*Deskripsi*: ${description}
-\n*Username*: ${username}`, m)
-conn.sendFile(m.chat, audio, 'tikmp3.opus', null, m)
-};
+let { tiktok } = require('api-dylux')
+let handler = async (m, {conn, text, command, usedPrefix}) => {
+    if (!text) throw `Masukkan Link Tiktok!`
+    let res = await tiktok(text)
+    conn.sendFile(m.chat, res.play, null, null, m)
+    conn.sendFile(m.chat, res.music. null, null, m)
+}
 handler.help = ['tiktok']
 handler.command = /^(tiktok|tt|tiktokdl|tiktoknowm|dltt)$/i
 handler.tags = ['downloader'];
